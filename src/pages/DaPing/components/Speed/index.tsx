36c9b5c64d615ded18ad3a0d27834b7a3079b01b
niaoshuai <niao.shuai123@163.com>
2022-06-06 14:32:54 +0800

    实时监控页面

--- src/pages/DaPing/components/Speed/index.tsx
+++ src/pages/DaPing/components/Speed/index.tsx
@@ -11,7 +11,7 @@ const DianLiang = () => {
     const myChart = echarts.init(divEl.current);
     myChart.setOption({
       title: {
-        text: '实时电量',
+        text: '实时速度',
         textStyle: {
           color: 'white',
         },
@@ -19,78 +19,50 @@ const DianLiang = () => {
       series: [
         {
           type: 'gauge',
-          startAngle: 180,
-          endAngle: 0,
-          min: 0,
-          max: 1,
-          splitNumber: 8,
           axisLine: {
             lineStyle: {
-              width: 6,
+              width: 30,
               color: [
-                [0.25, '#FF6E76'],
-                [0.5, '#FDDD60'],
-                [0.75, '#58D9F9'],
-                [1, '#7CFFB2'],
+                [0.3, '#67e0e3'],
+                [0.7, '#37a2da'],
+                [1, '#fd666d'],
               ],
             },
           },
           pointer: {
-            icon: 'path://M12.8,0.7l12,40.1H0.7L12.8,0.7z',
-            length: '12%',
-            width: 20,
-            offsetCenter: [0, '-60%'],
             itemStyle: {
               color: 'auto',
             },
           },
           axisTick: {
-            length: 12,
+            distance: -30,
+            length: 8,
             lineStyle: {
-              color: 'auto',
+              color: '#fff',
               width: 2,
             },
           },
           splitLine: {
-            length: 20,
+            distance: -30,
+            length: 30,
             lineStyle: {
-              color: 'auto',
-              width: 5,
+              color: '#fff',
+              width: 4,
             },
           },
           axisLabel: {
-            color: '#464646',
+            color: 'auto',
+            distance: 40,
             fontSize: 20,
-            distance: -60,
-            formatter(value) {
-              if (value === 0.875) {
-                return 'A';
-              } else if (value === 0.625) {
-                return 'B';
-              } else if (value === 0.375) {
-                return 'C';
-              } else if (value === 0.125) {
-                return 'D';
-              }
-              return '';
-            },
-          },
-          title: {
-            offsetCenter: [0, '-20%'],
-            fontSize: 30,
           },
           detail: {
-            fontSize: 50,
-            offsetCenter: [0, '0%'],
             valueAnimation: true,
-            formatter(value) {
-              return `${Math.round(value * 100)}%`;
-            },
+            formatter: '{value} km/h',
             color: 'auto',
           },
           data: [
             {
-              value: 0,
+              value: 70,
             },
           ],
         },
@@ -98,13 +70,13 @@ const DianLiang = () => {
     });
 
     setInterval(() => {
-      request('api/dianliang').then((data) => {
+      request('api/speed').then((data) => {
         myChart.setOption<echarts.EChartsOption>({
           series: [
             {
               data: [
                 {
-                  value: data.data.dianliang.toFixed(2),
+                  value: data.data.speed.toFixed(2),
                 },
               ],
             },
--- src/pages/DaPing/index.tsx
+++ src/pages/DaPing/index.tsx
@@ -1,18 +1,40 @@
 import { Box } from '@alifd/next';
 import * as React from 'react';
 import DianLiang from './components/DianLiang';
+import DianLiangQushi from './components/DianLiangQushi';
+import Speed from './components/Speed';
 import styles from './index.module.scss';
 
 const DaPing = () => {
   return (
-    <Box direction="row" align="center">
-      <Box className={styles.box180}>
-        <DianLiang />
-      </Box>
-      <Box className={styles.box180} />
-      <Box className={styles.box180} />
-      <Box className={styles.box180} />
-    </Box>
+    <div>
+      <div>
+        <Box align="center" >
+          <Box justify="center">
+            <h1 className={styles.colorWhite}>电摩实时监控大屏展示-【我的】</h1>
+          </Box>
+        </Box>
+      </div>
+      <div>
+        <Box direction="row" align="center">
+          <Box >
+            <DianLiang />
+          </Box>
+          <Box >
+            <DianLiangQushi />
+          </Box>
+          <Box >
+            <Speed />
+          </Box>
+        </Box>
+      </div>
+
+      <div>
+        <Box direction="row" align="center">
+          DIAOBAO
+        </Box>
+      </div>
+    </div>
   );
 };
 
